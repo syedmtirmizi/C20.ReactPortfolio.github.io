8a553e890aa037fbcba375d5294dd806ba452906
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-07-21 01:46:36 -0400

    Updates

---
+++ precache-manifest.b6810bd3e09b740496f4f2fcd8279f89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cac048cf8f8c49054ab3",
+    "url": "/C20.ReactPortfolio.github.io/static/css/main.22befe6c.chunk.css"
+  },
+  {
+    "revision": "cac048cf8f8c49054ab3",
+    "url": "/C20.ReactPortfolio.github.io/static/js/main.cac048cf.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/C20.ReactPortfolio.github.io/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4b7d1242dd76bbfa6d0f",
+    "url": "/C20.ReactPortfolio.github.io/static/js/runtime~main.4b7d1242.js"
+  },
+  {
+    "revision": "ed9cbc592b2e4d3b05471fca87d5c7f0",
+    "url": "/C20.ReactPortfolio.github.io/index.html"
+  }
+];
