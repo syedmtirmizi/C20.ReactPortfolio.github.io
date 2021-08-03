440cf592c7a8653588d55163f3aaf523164fc9b0
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-08-02 23:06:00 -0400

    update protfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ export default class Porfolio extends Component {
       <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Project Portfolio.</h1>
+          <h1>Project Portfolio</h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
           {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
